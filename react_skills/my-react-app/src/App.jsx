eb48b0e5c5d67c61cb7ba1edcefbe7506054cf1b
djibmsa@gmail.com <djibmsa@gmail.com>
2024-08-21 22:45:54 +0200

    scss

--- react_skills/my-react-app/src/App.jsx
+++ react_skills/my-react-app/src/App.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
-import './App.css';
-import UserProfile from './components/UserProfile';
-import UserContext from './contexts/UserContext';
+import React from "react";
+import "./style.scss";
 
 function App() {
-  const [isOnline, setIsOnline] = useState(false);
   return (
-    <>
-      <UserContext.Provider value={{ isOnline, setIsOnline }}>
-        <UserProfile />
-      </UserContext.Provider>
-    </>
+    <div>
+      <nav>
+        {/* use the class list to remove bullet points and align items side-by-side */}
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      {/* use the class title to change the title color */}
+      <h1>SCSS!</h1>
+      <p>This is an example using SCSS!</p>
+    </div>
   );
 }
 
