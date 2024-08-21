e39d9c5e5fd4f282373251346dd32a9fffd19e88
djibmsa@gmail.com <djibmsa@gmail.com>
2024-08-21 20:44:51 +0200

    userProfil update

--- react_skills/my-react-app/src/App.jsx
+++ react_skills/my-react-app/src/App.jsx
@@ -1,38 +1,16 @@
-import React from 'react';
-import UserCard from './components/userCard/userCard';
-import axios from 'axios';
-import {useState} from 'react';
-
-import User from './components/user';
-import Counter from './components/counter/counter';
-
-import "./App.css";
-
+import React, { useState } from 'react';
+import './App.css';
+import UserProfile from './components/UserProfile';
+import UserContext from './contexts/UserContext';
 
 function App() {
-  const [employee, setEmployee] = useState(null)
-  const getEmployee = () => {
-    // Send the request
-    axios
-      .get('https://randomuser.me/api?nat=en')
-      // Use this data to update the state
-      .then((response) => {
-        setEmployee(response.data.results[0]);
-      });
-  };
+  const [isOnline, setIsOnline] = useState(false);
   return (
-    <><div className="App">
-    {employee &&
-      <UserCard
-        firstname={employee.name.first}
-        lastname={employee.name.last}
-        image={employee.picture.large}
-        email={employee.email} />}
-    <button onClick={getEmployee}>click</button>
-  </div><div>
-      <User name="jason" />
-      <Counter title="Counter App" />
-    </div></>
+    <>
+      <UserContext.Provider value={{ isOnline, setIsOnline }}>
+        <UserProfile />
+      </UserContext.Provider>
+    </>
   );
 }
 
